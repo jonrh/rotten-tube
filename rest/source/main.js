7b590f9481ac1d4d1d7d55eddf2e2549dcf1840d
Jón Rúnar Helgason <jonrh@jonrh.is>
2015-11-06 17:09:44 +0000

    Fetching names from MySQL database is now working. Added node environment variables

--- rest/source/main.js
+++ rest/source/main.js
@@ -1,5 +1,11 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+    host     : process.env.mysql_host,
+    user     : process.env.my_user,
+    password : process.env.my_pass,
+    database : process.env.database_name
+});
 
 connection.connect();
 connection.query('select name from names', function(err, rows, fields){
