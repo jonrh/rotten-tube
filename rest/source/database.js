c1efcdc27cc961043d5ac0215a878bca9fd69803
Jillian <jillianbrett@hotmail.com>
2015-11-13 16:58:47 +0000

    Function getNameById and insertName were implemented in database file and for the REST server API

--- rest/source/database.js
+++ rest/source/database.js
@@ -28,7 +28,45 @@ var Database = {
                 });
             }
         );
+    },
+
+    getNameById: function(id){
+        return new Promise(
+            function(resolve, reject){
+                const queryStr ="select name from names where id ="+ id
+                connection.query(queryStr, function(error, rows, fields){
+                    if(error)
+                        reject(error);
+
+                    let name = rows[0].name;
+                    console.log(name);
+                    resolve(name);
+
+
+                });
+            }
+        );
+    } ,
+
+
+    insertName: function(name){
+        return new Promise(
+            function(resolve, reject){
+                const queryStr ="insert into names (name) values ('" + name + "')";
+                connection.query(queryStr, function(error, rows, fields){
+                    if(error)
+                        reject(error);
+
+
+                    console.log(rows);
+                    resolve(name);
+
+
+                });
+            }
+        );
     }
+
 };
 
 module.exports = Database;
--- rest/source/rest_server.js
+++ rest/source/rest_server.js
@@ -9,6 +9,15 @@ app.get("/names", function(request, response) {
     database.getAllNames().then(allNames => response.send(allNames));
 });
 
+app.get("/names/:id", function (request, response){
+    database.getNameById(request.params.id).then(name => response.send(name));
+});
+
+app.get("/namesx/", function (request, response) {
+    database.insertName("Samuel").then(data => response.send(data));
+});
+
+
 app.get('/wines', function(req, res) {
     res.send([{name:'wine1'}, {name:'wine2'}]);
 });
