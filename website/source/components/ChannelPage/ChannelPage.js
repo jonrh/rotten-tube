ad96bff7d6433e58ccc19d4654d8f8e138f2588f
Jón Rúnar Helgason <jonrh@jonrh.is>
2015-12-02 17:35:17 +0000

    Restructured layout to make things more compact on the Channel page

--- website/source/components/ChannelPage/ChannelPage.js
+++ website/source/components/ChannelPage/ChannelPage.js
@@ -2,8 +2,7 @@
 
 import React from "react";
 import { Button, Grid, Row, Col, Image, Input } from "react-bootstrap";
-const Paper = require('material-ui/lib/paper');
-const TextField = require('material-ui/lib/text-field');
+import Paper from "material-ui/lib/paper";
 
 import Review from "./Review";
 import { fetchReviews, fetchChannelByID, postReview } from "./../../data_fetching/rest.js";
@@ -66,59 +65,64 @@ class ChannelPage extends React.Component {
                         </a>
                     </Col>
                 </Row>
+
                 <Row>
                     <Col md={12}>
                         <h1>{this.state.channel.name}</h1>
                     </Col>
                 </Row>
+
                 <Row>
                     <Col md={4}>
-                        <iframe width="300" height="169" src="https://www.youtube.com/embed/UXS8UVJuGoI" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="100%" height="169" src="https://www.youtube.com/embed/iqhJvadqtAc" frameborder="0" allowfullscreen></iframe>
                     </Col>
                     <Col md={4}>
-                        <iframe width="300" height="169" src="https://www.youtube.com/embed/UXS8UVJuGoI" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="100%" height="169" src="https://www.youtube.com/embed/l6MJe5wKT7Y" frameborder="0" allowfullscreen></iframe>
                     </Col>
                     <Col md={4}>
-                        <iframe width="300" height="169" src="https://www.youtube.com/embed/0weCqOLSPfs" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="100%" height="169" src="https://www.youtube.com/embed/0weCqOLSPfs" frameborder="0" allowfullscreen></iframe>
                     </Col>
                 </Row>
+
                 <br />
 
-                <h2>Reviews:</h2>
-                <Paper zDepth={2} rounded={false}>
-                    {reviews}
-                </Paper>
-                <br />
-                <Paper zDepth={2} rounded={false}>
-
+                <Paper zDepth={5}>
                 <Row>
-                    <Col md={3}></Col>
-                    <Col md={6}>
+                    <Col md={5} className="littleBitOfMarginNeverHurtNobody">
+                        <h2>Reviews</h2>
+                        <Paper zDepth={1}>
+                            {reviews}
+                        </Paper>
+                    </Col>
 
+                    <Col md={5} className="littleBitOfMarginNeverHurtNobody">
                         <h2>Write a review</h2>
-                        <Input type="text" label="Name" ref="name" placeholder="Put your name here sure" />
 
-                        <Input type="select" label="Rating" ref="rating" placeholder="select">
-                            <option value="0">select</option>
-                            <option value="1">1 - Terrible.</option>
-                            <option value="2">2 - Not worth my time</option>
-                            <option value="3">3 - Radha's love for the Irish climate</option>
-                            <option value="4">4 - Not my cup of tea</option>
-                            <option value="5">5 - It's ok</option>
-                            <option value="6">6 - I wouldn't kick it out of bed </option>
-                            <option value="7">7 - I like it </option>
-                            <option value="8">8 - Dog's bollocks</option>
-                            <option value="9">9 - I love it</option>
-                            <option value="10">10 - Better than Rems teaching (Impossible)</option>
-                        </Input>
-                        <Input type="textarea" label="Comment" ref="comment" placeholder="Got something to say ??" />
-                        <Button onClick={this.handleReviewSubmit.bind(this)} active>Submit Review</Button>
+                        <Paper zDepth={1}>
+                            <div className="littleBitOfPaddingNeverHurtNobody">
+                                <Input type="text" label="Name" ref="name" placeholder="Put your name here sure" />
 
+                                <Input type="select" label="Rating" ref="rating" placeholder="select">
+                                    <option value="0">select</option>
+                                    <option value="1">1 - Terrible.</option>
+                                    <option value="2">2 - Not worth my time</option>
+                                    <option value="3">3 - Radha's love for the Irish climate</option>
+                                    <option value="4">4 - Not my cup of tea</option>
+                                    <option value="5">5 - It's ok</option>
+                                    <option value="6">6 - I wouldn't kick it out of bed </option>
+                                    <option value="7">7 - I like it </option>
+                                    <option value="8">8 - Dog's bollocks</option>
+                                    <option value="9">9 - I love it</option>
+                                    <option value="10">10 - Better than Rems teaching (Impossible)</option>
+                                </Input>
+                                <Input type="textarea" label="Comment" ref="comment" placeholder="Got something to say ??" />
+                                <Button onClick={this.handleReviewSubmit.bind(this)} active>Submit Review</Button>
+                            </div>
+                        </Paper>
                     </Col>
-                    <Col md={3}></Col>
                 </Row>
-                    <br />
                 </Paper>
+
             </Grid>
         );
     }
--- website/source/main.js
+++ website/source/main.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
-ReactDOM.render(
-    <App name="jónsi" />,
-    document.getElementById('app_content')
-);
+// Tells React to use the <div> with ID "app_content". React will then change
+// and modify this div as needed.
+ReactDOM.render(<App/>, document.getElementById('app_content'));
